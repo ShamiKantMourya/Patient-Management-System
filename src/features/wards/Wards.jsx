503eac20e134f4eee38df0f5c23ea8b45b5fa2aa
Shami_Mourya <shamimourya9852@gmail.com>
2024-05-15 12:42:12 +0530

    Ward details

---
+++ src/features/wards/Wards.jsx
@@ -0,0 +1,25 @@
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+
+import WardForm from "../../components/WardForm";
+import { addWards } from "./wardSlice";
+
+export default function Wards() {
+  const { wards } = useSelector((state) => state.wards);
+  return (
+    <div>
+      <h3>Wards View</h3>
+      <WardForm type="add" submitFunction={addWards} />
+      <ul>
+        {wards?.map((item) => (
+          <li key={item._id}>
+            <NavLink to={`/wards/${item._id}`}>
+              Ward Number - {item.wardNumber} <br /> Specialization - (
+              {item.specialization})
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
