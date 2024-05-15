28024061ba02594df882a0176c1d369edfe0fb88
Shami_Mourya <shamimourya9852@gmail.com>
2024-05-15 12:41:33 +0530

    Patient data

--- src/features/patients/Patients.jsx
+++ src/features/patients/Patients.jsx
@@ -1,24 +1,25 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import PatientForm from "../../components/PatientsForm";
 
-import {addPatients} from './patientSlice'
+import { addPatients } from "./patientSlice";
 
 export default function Patient() {
-  const { patients } = useSelector(state => state.patients);
+  const { patients } = useSelector((state) => state.patients);
 
-  return <div>
-    <h3>Patients View</h3> 
-     <PatientForm submitFunction={addPatients} type="add"/>
-    <ul>
-      {
-        patients?.map(item => <li key={item._id}>
-          <NavLink to={`/patients/${item._id}`}>
-         <span>{item.name}</span>  ({item.age})
-          </NavLink>
-          </li>)
-      }
-    </ul>
-  
-  </div>
+  return (
+    <div>
+      <h3>Patients View</h3>
+      <PatientForm submitFunction={addPatients} type="add" />
+      <ul>
+        {patients?.map((item) => (
+          <li key={item._id}>
+            <NavLink to={`/patients/${item._id}`}>
+              <span>{item.name}</span> ({item.age})
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
